d8cc09bb8b7200a273bd47cd90f5a27c4f10c15c
hvirgue <103527324+hvirgue@users.noreply.github.com>
2024-11-17 18:32:34 -0500

    itegrate with release semana 6

--- kraken-e2e/features/web/step_definitions/step.js
+++ kraken-e2e/features/web/step_definitions/step.js
@@ -1,5 +1,4 @@
-const { Given, When, Then } = require('@cucumber/cucumber');
-var assert = require('assert-plus');
+const { When} = require('@cucumber/cucumber');
 
 When('I login in ghost {kraken-string} {kraken-string}', async function (email, password){
 
@@ -171,38 +170,7 @@ When('I perform the final review', async function () {
 });
 
 
-Then('I click on the created post titled {string}', async function (pageTitle) {
-    const postLink = await this.driver.$(`a[title="View post: ${pageTitle}"]`);
 
-    await postLink.scrollIntoView();
-    await postLink.waitForDisplayed({ timeout: 10000 });
-    await postLink.waitForClickable({ timeout: 10000 });
-
-    await postLink.click();
-
-    await this.driver.waitUntil(async () => {
-        const url = await this.driver.getUrl();
-        return url.includes(pageTitle.toLowerCase().replace(/\s+/g, '-'));
-    }, { timeout: 10000, timeoutMsg: 'La navegación a la nueva página no se completó.' });
-
-    console.log(`Se ha hecho clic en el post titulado "${pageTitle}" y se ha navegado a la nueva página.`);
-});
-
-Then('I should see the new page', async function () {
-    const youtubeIframe = await this.driver.$('iframe[src*="youtube.com"]');
-
-    if (!await youtubeIframe.isExisting()) {
-        throw new Error('El video de YouTube no se encuentra en la página.');
-    }
-
-    await youtubeIframe.waitForDisplayed({ timeout: 15000 });
-
-    if (!await youtubeIframe.isDisplayed()) {
-        throw new Error('El video de YouTube no está visible en la página.');
-    }
-
-    console.log('El video de YouTube está presente en la página.');
-});
 
 When('I add HTML content {string} to the page', async function (htmlContent) {
     const droppableParagraph = await this.driver.$('p[data-koenig-dnd-droppable="true"]');
@@ -279,85 +247,6 @@ When('I update the page', async function () {
     await updateButton.click();
 });
 
-Then('I open page updated', async function () {
-    const viewPostLink = await this.driver.$('.gh-editor-post-status .view-post');
-    await viewPostLink.waitForDisplayed({ timeout: 5000 });
-    await viewPostLink.click();
-    const windows = await this.driver.getWindowHandles();
-    await this.driver.switchToWindow(windows[windows.length - 1]);
-
-    console.log('La página se abrió correctamente.');
-});
-
-Then('I should see the updated page with the new title and content', async function () {
-
-    const windows = await this.driver.getWindowHandles();
-    await this.driver.switchToWindow(windows[windows.length - 1]);
-
-    const updatedTitle = await this.driver.$('h1');
-    const updatedContent = await this.driver.$('p');
-
-    if (await updatedTitle.getText() !== 'Updated Page Title') {
-        throw new Error('El título actualizado no es visible.');
-    }
-
-    if (await updatedContent.getText() !== 'New paragraph.') {
-        throw new Error('El contenido actualizado no es visible.');
-    }
-
-    console.log('La página fue actualizada correctamente.');
-});
-
-Then('I should see the title {string} and html {string} description in the post', async function (title, html) {
-
-    const windows = await this.driver.getWindowHandles();
-    await this.driver.switchToWindow(windows[windows.length - 1]);
-
-    const updatedTitle = await this.driver.$('h1');
-    const updatedContent = await this.driver.$('h2');
-
-    if (await updatedTitle.getText() !== title) {
-        throw new Error('El título actualizado no es visible.');
-    }
-
-    if (await updatedContent.getText() !== html) {
-        throw new Error('El contenido actualizado no es visible.');
-    }
-
-    console.log('La página con html fue creada exitosamente.');
-});
-
-Then('I should see the tag {string} in the post', async function (tagName) {
-
-    const windows = await this.driver.getWindowHandles();
-    await this.driver.switchToWindow(windows[windows.length - 1]);
-
-    const updatedTag = await this.driver.$('a[class="gh-article-tag"]');
-
-    assert.equal(await updatedTag.getText(),tagName.toUpperCase(),'El tag no coincide');
-
-    console.log('El post fue creada exitosamente.');
-});
-
-Then('I should see the title {string} and html {string} description in the page', async function (title, html) {
-
-    const windows = await this.driver.getWindowHandles();
-    await this.driver.switchToWindow(windows[windows.length - 1]);
-
-    const updatedTitle = await this.driver.$('h1');
-    const updatedContent = await this.driver.$('h2');
-
-    if (await updatedTitle.getText() !== title) {
-        throw new Error('El título actualizado no es visible.');
-    }
-
-    if (await updatedContent.getText() !== html) {
-        throw new Error('El contenido actualizado no es visible.');
-    }
-
-    console.log('La página con html fue creada exitosamente.');
-});
-
 When('I return pages', async function () {
     await this.driver.url('http://localhost:2368/ghost/#/dashboard');
     await this.driver.pause(200)
@@ -402,27 +291,6 @@ When('I delete the post {string}', async function (postTitle) {
     await this.driver.pause(2000);
 });
 
-Then('I should see that the page {string} is deleted', async function (pageTitle) {
-    const pageExists = await this.driver.$(`//h3[contains(@class, "gh-content-entry-title") and text()="${pageTitle}"]`).isExisting();
-
-    if (pageExists) {
-        throw new Error(`La página con título "${pageTitle}" aún existe después de intentar eliminarla.`);
-    }
-
-    console.log(`La página con título "${pageTitle}" se ha eliminado correctamente.`);
-});
-
-Then('I verify that no YouTube videos or links are visible', async function () {
-    const youtubeLinks = await this.driver.$$('a[href*="youtube.com"]');
-    const embeddedVideos = await this.driver.$$('iframe[src*="youtube.com"]');
-
-    if (youtubeLinks.length > 0 || embeddedVideos.length > 0) {
-        throw new Error('Se encontraron enlaces o videos de YouTube en la página de error');
-    }
-
-    console.log('La página de error se mostró correctamente y no hay videos ni enlaces de YouTube.');
-});
-
 When('I navigate to Tags module', async function () {
     const pagesModule = await this.driver.$('[data-test-nav="tags"]');
     await pagesModule.waitForDisplayed({ timeout: 5000 });
@@ -463,64 +331,6 @@ When('I save the new tag', async function () {
     await saveButton.click();
 });
 
-
-//Esenario 6
-Given('I navigate to the posts section', async function () {
-    await this.driver.url('http://localhost:2368/ghost/#/posts');
-    let url = await this.driver.getUrl();
-    if (!url.includes('/posts')) {
-        throw new Error('Navigation to posts section failed');
-    }
-});
-
-Given('I validate delete post publish available', async function () {
-    try{
-        let postList = await this.driver.$('a.gh-post-list-button');
-        if (postList.length === 0) {
-            console.log('No hay posts disponibles, pasando al siguiente escenario.');
-            return 'skipped';
-        }
-        await postList.click();
-        let settingsButton = await this.driver.$('button[data-test-psm-trigger]');
-        await settingsButton.click();
-        let panel = await this.driver.$('.settings-menu-pane-main');
-        await panel.scrollIntoView();
-
-        let deleteButton = await this.driver.$('button[data-test-button="delete-post"]');
-        await deleteButton.click();
-        let confirmDeleteButton = await this.driver.$('button[data-test-button="delete-post-confirm"]');
-        await confirmDeleteButton.click();
-    }catch(error) {
-        console.log('Error al validar la existencia de posts:', error);
-    }
-});
-
-//escenario 7
-Given('I validate one post publish available', async function () {
-    let postAvailable = await this.driver.$('a.gh-post-list-button');
-    if (postAvailable.length === 0) {
-        throw new Error('No posts available to delete');
-    }
-    await postAvailable.click();
-});
-
-Given('I delete post publish available', async function () {
-    let optionButton = await this.driver.$('button[data-test-button="analytics-actions"]');
-    await optionButton.click();
-    let deleteButton = await this.driver.$('button[data-test-button="delete-post"]');
-    await deleteButton.click();
-    let confirmDeleteButton = await this.driver.$('button[data-test-button="delete-post-confirm"]');
-    await confirmDeleteButton.click();
-});
-
-Then('I should be redirected to the posts page', async function () {
-    let url = await this.driver.getUrl();
-    if (!url.includes('/posts')) {
-        throw new Error('Did not redirect to posts page');
-    }
-});
-
-//Escenario 8
 When('I click on new story', async function () {
     let element = await this.driver.$('a[data-test-nav="new-story"]');
     await element.click();
@@ -560,7 +370,6 @@ When('I publish the post', async function () {
     await confirmButton.click();
 });
 
-//Escenario 9
 When('I add an HTML card with content {string}', async function (htmlContent) {
     let addCardButton = await this.driver.$('button[aria-label="Add a card"]');
     await addCardButton.click();
@@ -570,7 +379,6 @@ When('I add an HTML card with content {string}', async function (htmlContent) {
     await htmlInput.setValue(htmlContent);
 });
 
-//Escenario 10
 When('I click on new page', async function () {
     let newPageButton = await this.driver.$('a[data-test-new-page-button]');
     await newPageButton.click();
@@ -591,35 +399,6 @@ When('I publish the page html', async function () {
     let confirmButton = await this.driver.$('button[data-test-button="confirm-publish"]');
     await confirmButton.click();
 });
-
-Then('I should be redirected to the pages page', async function () {
-    let url = await this.driver.getUrl();
-    if (!url.includes('/pages')) {
-        throw new Error('Did not redirect to pages page');
-    }
-});
-
-Then('The page with the tag should be created {string}', async function (text) {
-    const newtagInpost = await this.driver.$(`//span[contains(text(), "${text}" )]`);
-    const istagPostCreated = await newtagInpost.isDisplayed();
-
-    if (!istagPostCreated) {
-        throw new Error('El nuevo post con el tag no fue creado correctamente.');
-    }
-
-    console.log('El nuevo post con el tag fue creado correctamente.');
-})
-
-Then('The post with the tag should be created {string}', async function (text) {
-    const newtagInpage = await this.driver.$(`//span[contains(text(), "${text}" )]`);
-    const istagPageCreated = await newtagInpage.isDisplayed();
-
-    if (!istagPageCreated) {
-        throw new Error('la nueva post con el tag no fue creado correctamente.');
-    }
-
-    console.log('la nueva page con el tag fue creado correctamente.');
-})
 
 When('I click on tags link', async function () {
     let element = await this.driver.$('a[data-test-nav="tags"]');
@@ -685,17 +464,6 @@ When('I save the new member', async function () {
     return await element.click();
 })
 
-Then('The new member should be created {string}', async function (text) {
-    const newMemberInList = await this.driver.$(`//p[contains(text(), "${text}" )]`);
-    const isMemberCreated = await newMemberInList.isDisplayed();
-
-    if (!isMemberCreated) {
-        throw new Error('El nuevo member no fue creado correctamente.');
-    }
-
-    console.log('El nuevo member fue creado correctamente.');
-});
-
 When('I deleted the new member {string}', async function (memberemail) {
     const backToMembersLink = await this.driver.$('[data-test-link="members-back"]');
     await backToMembersLink.click();
@@ -715,17 +483,6 @@ When('I deleted the new member {string}', async function (memberemail) {
     let clickConfirmDelete = await this.driver.$('button[data-test-button="confirm"]');
     await clickConfirmDelete.click();
 
-})
-
-Then('The member should be deleted {string}', async function (text) {
-    const newMemberInList = await this.driver.$(`//p[contains(text(), "${text}" )]`);
-    const isMemberCreated = await newMemberInList.isDisplayed();
-
-    if (isMemberCreated) {
-        throw new Error('El nuevo member no fue eliminado correctamente.');
-    }
-
-    console.log('El nuevo member fue eliminado correctamente.');
 })
 
 When('I navigate to View Site', async function () {
@@ -754,96 +511,3 @@ When('I save changes to of site', async function() {
     await saveButton.click();
 });
 
-Then('The page with the title site should be {string}', async function(expectedTitle) {
-    await this.driver.url('http://localhost:2368/');
-
-    let titleElement = await this.driver.$('a.gh-navigation-logo.is-title');
-    let actualTitle = await titleElement.getText();
-
-    if (actualTitle !== expectedTitle) {
-        throw new Error(`Title mismatch: expected "${expectedTitle}", but got "${actualTitle}"`);
-    }
-});
-
-Then('I should see the post with title {string}', async function (expectedTitle) {
-    const windows = await this.driver.getWindowHandles();
-    await this.driver.switchToWindow(windows[windows.length - 1]);
-    const postTitle = await this.driver.$('h1.gh-article-title.is-title');
-    const titleText = await postTitle.getText();
-
-    if (titleText !== expectedTitle) {
-        throw new Error(`Expected title to be "${expectedTitle}", but got "${titleText}"`);
-    }
-});
-
-Then('I should see the post with text {string}', async function (expectedText) {
-    const windows = await this.driver.getWindowHandles();
-    await this.driver.switchToWindow(windows[windows.length - 1]);
-    const postText = await this.driver.$('section.gh-content p strong');
-    const text = await postText.getText();
-
-    if (text !== expectedText) {
-        throw new Error(`Expected text to be "${expectedText}", but got "${text}"`);
-    }
-});
-
-Then('I shouldn´t see the post with title {string}', async function (text) {
-    const postDeleted = await this.driver.$(`//h3[contains(text(), "${text}" )]`);
-    const isPostDeleted = await postDeleted.isDisplayed();
-
-    if (isPostDeleted) {
-        throw new Error('El draft del post no se elimino correctamente');
-    }
-
-    console.log('El draft del post se elimino correctamente');
-})
-
-
-Then('I should see the post with title {string} and an Unsplash image', async function (expectedTitle) {
-    const windows = await this.driver.getWindowHandles();
-    await this.driver.switchToWindow(windows[windows.length - 1]);
-
-    await this.driver.pause(2000);
-
-    const postTitleElement = await this.driver.$('h1.gh-article-title.is-title');
-    const titleText = await postTitleElement.getText();
-
-    if (titleText !== expectedTitle) {
-        throw new Error(`Expected title to be "${expectedTitle}", but got "${titleText}"`);
-    }
-
-    const postImage = await this.driver.$('figure.gh-article-image img[src*="images.unsplash.com"]');
-    const imageExists = await postImage.isExisting();
-
-    if (!imageExists) {
-        throw new Error('Expected an Unsplash image in the post, but none was found.');
-    }
-});
-
-Then('I should see the post with title {string} and content {string}', async function (expectedTitle, expectedContent) {
-    const windows = await this.driver.getWindowHandles();
-    await this.driver.switchToWindow(windows[windows.length - 1]);
-
-    await this.driver.pause(2000);
-
-    const postTitleElement = await this.driver.$('h1.gh-article-title.is-title');
-    const titleText = await postTitleElement.getText();
-    if (titleText !== expectedTitle) {
-        throw new Error(`Expected title to be "${expectedTitle}", but got "${titleText}"`);
-    }
-
-    const postContentElement = await this.driver.$('section.gh-content p');
-    const contentText = await postContentElement.getText();
-    if (contentText !== expectedContent) {
-        throw new Error(`Expected content to be "${expectedContent}", but got "${contentText}"`);
-    }
-});
-
-Then('I expect to see tag {string}', async function (expectedText) {
-    const elementSelector = '.gh-tag-list-name';
-    const element = await this.driver.$(elementSelector);
-    const actualText = await element.getText();
-
-    assert.ok(actualText.includes(expectedText),`Expected to find text "${expectedText}" in element "${elementSelector}", but found "${actualText}"`)
-
-});
