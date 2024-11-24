6c8ef5629dc94aadf9749d6086abab96b25134ed
hvirgue <103527324+hvirgue@users.noreply.github.com>
2024-11-24 09:13:47 -0500

    order test

--- cypress-e2e/cypress/e2e/ghost_e2e_harold.test.cy.js
+++ cypress-e2e/cypress/e2e/ghost_e2e_harold.test.cy.js
@@ -12,7 +12,7 @@ describe('Crear un post', () => {
         Given.givenNavigateDashboard();
     });
 
-   it('Escenario 11: Crear y editar una página en Ghost', () => {
+   it('Escenario 1: Crear y editar una página en Ghost', () => {
         // GIVEN: Visitar Pages
         Given.givenNavigateToPagePage();
 
@@ -22,8 +22,7 @@ describe('Crear un post', () => {
         // THEN: Publicar la página
         Then.validatePageWasEditedAndCreated();
     });
-
-    it('Escenario 10: Crear y publicar una página con un video de YouTube', () => {
+    it('Escenario 2: Crear y publicar una página con un video de YouTube', () => {
         // GIVEN: Visitar Pages
         Given.givenNavigateToPagePage();
 
@@ -33,8 +32,7 @@ describe('Crear un post', () => {
         // THEN: Validar la publicación de la página
         Then.validatePageWithVideoCreated();
     });
-
-    it('Escenario 11: Crear, publicar y eliminar una página en Ghost', () => {
+    it('Escenario 3: Crear, publicar y eliminar una página en Ghost', () => {
 
         // GIVEN: Visitar Pages
         Given.givenNavigateToPagePage();
@@ -46,8 +44,7 @@ describe('Crear un post', () => {
         Then.validatePageWasCreatedAndDeleted();
 
     });
-
-    it('Escenario 12: Crear una pagina y agregar un link de youtube invalido', () =>{
+    it('Escenario 4: Crear una pagina y agregar un link de youtube invalido', () =>{
         // GIVEN: Visitar Pages
         Given.givenNavigateToPagePage();
 
@@ -57,8 +54,7 @@ describe('Crear un post', () => {
         Then.validatePageWasCreateWithWrongVideo();
 
     })
-
-    it('Escenario 13: Crear un tag', () => {
+    it('Escenario 5: Crear un tag', () => {
         // GIVEN: Visitar Tags
         Given.givenNavigateToTagsPage();
 
@@ -68,8 +64,7 @@ describe('Crear un post', () => {
         // THEN: Validar la creación del tag
         Then.validateTagWasCreated();
     });
-
-    it('Escenario 14: Crear y publicar una página con un video de YouTube en la que el título tenga más de 254 caracteres.', () => {
+    it('Escenario 6: Crear y publicar una página con un video de YouTube en la que el título tenga más de 254 caracteres.', () => {
         // THEN: Visitar pagina de posts
         Given.givenNavigateToPagePage();
 
@@ -79,8 +74,7 @@ describe('Crear un post', () => {
         // THEN: Validar la publicación de la página
         Then.seeAlertMessage();
     });
-
-        it('Escenario 15: Crear una página con un título vacío y un video embebido.', () => {
+    it('Escenario 7: Crear una página con un título vacío y un video embebido.', () => {
         // GIVEN: Visitar Pages
         Given.givenNavigateToPagePage();
 
@@ -90,8 +84,7 @@ describe('Crear un post', () => {
         // THEN: Validar la publicación de la página
         Then.validatePageWithTitleNumberCreated();
     });
-
-    it('Escenario 16: Crear múltiples etiquetas con el mismo nombre.', () => {
+    it('Escenario 8: Crear múltiples etiquetas con el mismo nombre.', () => {
         // GIVEN: Visitar Tags
         Given.givenNavigateToTagsPage();
 
@@ -104,29 +97,7 @@ describe('Crear un post', () => {
         Then.validateTagWasCreatedName("Tecnología");
         Then.validateTagWasCreatedName("Ciencia");
     });
-
-         it('Escenario 17: Crear una página con un título que incluye caracteres especiales como #@$%^&*.', () => {
-        // THEN: Visitar pagina de posts
-        Given.givenNavigateToPagePage();
-
-        // WHEN: Crear y publicar post
-        When.createAndPublishPageWithHtmlPool(2);
-
-        // THEN: Verificar post publicado
-        Then.seePagePublishedWithHtml();
-    });
-    it('Escenario 18: Crear una etiqueta con un nombre largo+ 200', () => {
-        // GIVEN: Visitar Tags
-        Given.givenNavigateToTagsPage();
-
-        // WHEN: Crear publicar nuevo tag
-        When.createNewTagPool(2);
-
-        // THEN: Validar la creación del tag
-        Then.validateErrorTag();
-    });
-
-    it('Escenario 19: Crear una etiqueta con emojis y símbolos especiales', () => {
+    it('Escenario 9: Crear una etiqueta con emojis y símbolos especiales', () => {
         // GIVEN: Visitar Tags
         Given.givenNavigateToTagsPage();
 
@@ -136,8 +107,7 @@ describe('Crear un post', () => {
         // THEN: Validar la creación del tag
         Then.validateTagWasCreatedName("Etiqueta 🌟 #Test @123");
     });
-
-    it('Escenario 20: Crear una etiqueta con un nombre de exactamente 190 caracteres.', () => {
+    it('Escenario 10: Crear una etiqueta con un nombre de exactamente 190 caracteres.', () => {
         // GIVEN: Visitar Tags
         Given.givenNavigateToTagsPage();
 
@@ -147,8 +117,7 @@ describe('Crear un post', () => {
         // THEN: Validar la creación del tag
         Then.validateTagWasCreatedName("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
     });
-
-    it('Escenario 21: Crear una etiqueta con una descripción de exactamente 500 caracteres.', () => {
+    it('Escenario 11: Crear una etiqueta con una descripción de exactamente 500 caracteres.', () => {
         // GIVEN: Visitar Tags
         Given.givenNavigateToTagsPage();
 
@@ -158,8 +127,7 @@ describe('Crear un post', () => {
         // THEN: Validar la creación del tag
         Then.validateTagWasCreatedName("Etiqueta con descripción larga");
     });
-
-    it('Escenario 22: Crear una etiqueta con una descripción de exactamente 501 caracteres.', () => {
+    it('Escenario 12: Crear una etiqueta con una descripción de exactamente 501 caracteres.', () => {
         // GIVEN: Visitar Tags
         Given.givenNavigateToTagsPage();
 
@@ -169,8 +137,7 @@ describe('Crear un post', () => {
         // THEN: Validar error del tag
         Then.validateErrorDescriptionTag();
     });
-
-    it('Escenario 23: Crear una nombre de 1 caracter.', () => {
+    it('Escenario 13: Crear una nombre de 1 caracter.', () => {
         // GIVEN: Visitar Tags
         Given.givenNavigateToTagsPage();
 
@@ -180,8 +147,7 @@ describe('Crear un post', () => {
         // THEN: Validar la creación del tag
         Then.validateTagWasCreatedName("1");
     });
-
-        it('Escenario 24: Crear y editar una página en Ghost Faker', () => {
+    it('Escenario 14: Crear y editar una página en Ghost Faker', () => {
         // GIVEN: Visitar Pages
         Given.givenNavigateToPagePage();
 
@@ -191,8 +157,7 @@ describe('Crear un post', () => {
         // THEN: Publicar la página
         Then.seePageUpdatedWithFaker();
     });
-
-    it('Escenario 25: Crear y publicar una página con un video de YouTube FAKER', () => {
+    it('Escenario 15: Crear y publicar una página con un video de YouTube FAKER', () => {
         // GIVEN: Visitar Pages
         Given.givenNavigateToPagePage();
 
@@ -202,8 +167,7 @@ describe('Crear un post', () => {
         // THEN: Validar la publicación de la página
         Then.seePagePublishedWithVideoFaker();
     });
-
-    it('Escenario 26: Crear, publicar y eliminar una página en Ghost', () => {
+    it('Escenario 16: Crear, publicar y eliminar una página en Ghost faker', () => {
 
         // GIVEN: Visitar Pages
         Given.givenNavigateToPagePage();
@@ -215,8 +179,7 @@ describe('Crear un post', () => {
         Then.validatePageWasCreatedAndDeleted();
 
     });
-
-         it('Escenario 27: Crear una página con un título que incluye caracteres especiales como #@$%^&*. Faker', () => {
+    it('Escenario 17: Crear una página con un título que incluye caracteres especiales como #@$%^&*. Faker', () => {
         // THEN: Visitar pagina de posts
         Given.givenNavigateToPagePage();
 
@@ -226,8 +189,7 @@ describe('Crear un post', () => {
         // THEN: Verificar post publicado
         Then.seePagePublishedWithHtmlFaker();
     });
-
-        it('Escenario 28: Crear un tag usando faker', () => {
+    it('Escenario 18: Crear un tag usando faker', () => {
         // GIVEN: Visitar Tags
         Given.givenNavigateToTagsPage();
 
@@ -237,8 +199,7 @@ describe('Crear un post', () => {
         // THEN: Validar la creación del tag
         Then.validateTagWasCreatedFaker();
     });
-
-    it('Escenario 29: Crear una etiqueta nueva y verificar su creación en el módulo de tags.', () => {
+    it('Escenario 19: Crear una etiqueta nueva y verificar su creación en el módulo de tags mockaroo.', () => {
         // GIVEN: Visitar Tags
         Given.givenNavigateToTagsPage();
 
@@ -248,8 +209,7 @@ describe('Crear un post', () => {
         // THEN: Validar la creación del tag
         Then.validateTagWasCreatedName("aasuscipit nulla");
     });
-
-        it('Escenario 30: Crear un tag usando faker execendiendo maximo de caracteres', () => {
+    it('Escenario 20: Crear un tag usando faker execendiendo maximo de caracteres', () => {
         // GIVEN: Visitar Tags
         Given.givenNavigateToTagsPage();
 
@@ -259,8 +219,7 @@ describe('Crear un post', () => {
         // THEN: Validar la creación del tag
         Then.validateErrorTag();
     });
-
-        it('Escenario 31: Crear y publicar una página con un video de YouTube Mockaroo', () => {
+    it('Escenario 21: Crear y publicar una página con un video de YouTube Mockaroo', () => {
         // GIVEN: Visitar Pages
         Given.givenNavigateToPagePage();
 
@@ -270,8 +229,7 @@ describe('Crear un post', () => {
         // THEN: Validar la publicación de la página
         Then.validatePageWithVideoCreatedName("volutpat sapien arcu sed augue");
     });
-
-    it('Escenario 32: Crear y publicar una página con un video de YouTube Mockaroo', () => {
+    it('Escenario 22: Crear y actualziar una pagina con datos generados en Mockaroo', () => {
         // THEN: Visitar pagina de posts
         Given.givenNavigateToPagePage();
 
@@ -281,8 +239,97 @@ describe('Crear un post', () => {
         // THEN: Validar la publicación de la página
         Then.validatePageWasEditedAndCreated("lectus in quam fringilla rhoncus mauris enim leo rhoncus sed");
     });
+    it('Escenario 23: Crear una página con un título que incluye caracteres especiales como #@$%^&*.', () => {
+        // THEN: Visitar pagina de posts
+        Given.givenNavigateToPagePage();
 
-        it('Escenario 33: Crear, publicar y eliminar una página en Ghost Mockaroo', () => {
+        // WHEN: Crear y publicar post
+        When.createAndPublishPageWithHtmlPool(2);
+
+        // THEN: Verificar post publicado
+        Then.seePagePublishedWithHtml();
+    });
+    it('Escenario 24: Crear una etiqueta con una descripción que incluya caracteres especiales (#@$%^&*).', () => {
+        // GIVEN: Visitar Tags
+        Given.givenNavigateToTagsPage();
+
+        // WHEN: Crear publicar nuevo tag
+        When.createNewTagPool(8);
+
+        // THEN: Validar la creación del tag
+        Then.validateTagWasCreatedName("Etiqueta con descripción larga");
+    });
+    it('Escenario 25: Crear y editar una página en Ghost para que el titulo tenga emojis', () => {
+            // GIVEN: Visitar Pages
+            Given.givenNavigateToPagePage();
+
+            // WHEN: Crear publicar, editar y guardar
+            When.createAndPublishPageEditAndSavePool(3);
+
+            // THEN: Publicar la página
+            Then.validatePageWasEditedAndCreated();
+        });
+    it('Escenario 26: Crear una página con HTML que incluya tablas.', () => {
+            // THEN: Visitar pagina de posts
+            Given.givenNavigateToPagePage();
+
+            // WHEN: Crear y publicar post
+            When.createAndPublishPageWithHtmlPool(3);
+
+            // THEN: Verificar post publicado
+            Then. seePagePublishedWithHtmlName("Pagina con tabla HTML")
+        });
+    it('Escenario 27: Crear una página con HTML que incluya una lista.', () => {
+            // THEN: Visitar pagina de posts
+            Given.givenNavigateToPagePage();
+
+            // WHEN: Crear y publicar post
+            When.createAndPublishPageWithHtmlPool(4);
+
+            // THEN: Verificar post publicado
+            Then. seePagePublishedWithHtmlName("Página con lista HTML")
+        });
+    it('Escenario 28: Crear una página con HTML con imagen y parrafo.', () => {
+            // THEN: Visitar pagina de posts
+            Given.givenNavigateToPagePage();
+
+            // WHEN: Crear y publicar post
+            When.createAndPublishPageWithHtmlPool(5);
+
+            // THEN: Verificar post publicado
+            Then. seePagePublishedWithHtmlName("The Rise of Sustainable Energy")
+        });
+    it('Escenario 29: Crear una página con HTML  con enlace y párrafos.', () => {
+            // THEN: Visitar pagina de posts
+            Given.givenNavigateToPagePage();
+
+            // WHEN: Crear y publicar post
+            When.createAndPublishPageWithHtmlPool(6);
+
+            // THEN: Verificar post publicado
+            Then. seePagePublishedWithHtmlName("Exploring the Depths of the Ocean")
+        });
+    it('Escenario 30: Crear una página con  HTML con video y formulario de contacto.', () => {
+            // THEN: Visitar pagina de posts
+            Given.givenNavigateToPagePage();
+
+            // WHEN: Crear y publicar post
+            When.createAndPublishPageWithHtmlPool(7);
+
+            // THEN: Verificar post publicado
+            Then. seePagePublishedWithHtmlName("How to Start Your Own Business")
+        });
+    it('Escenario 31: Crear una etiqueta con un nombre largo+ 200', () => {
+        // GIVEN: Visitar Tags
+        Given.givenNavigateToTagsPage();
+
+        // WHEN: Crear publicar nuevo tag
+        When.createNewTagPool(2);
+
+        // THEN: Validar la creación del tag
+        Then.validateErrorTag();
+    });
+    it('Escenario 32: Crear, publicar y eliminar una página en Ghost Mockaroo', () => {
 
         // GIVEN: Visitar Pages
         Given.givenNavigateToPagePage();
@@ -295,79 +342,5 @@ describe('Crear un post', () => {
 
     });
 
-    it('Escenario 34: Crear una etiqueta con una descripción que incluya caracteres especiales (#@$%^&*).', () => {
-        // GIVEN: Visitar Tags
-        Given.givenNavigateToTagsPage();
-
-        // WHEN: Crear publicar nuevo tag
-        When.createNewTagPool(8);
-
-        // THEN: Validar la creación del tag
-        Then.validateTagWasCreatedName("Etiqueta con descripción larga");
-    });
-
-        it('Escenario 35: Crear y editar una página en Ghost para que el titulo tenga emojis', () => {
-        // GIVEN: Visitar Pages
-        Given.givenNavigateToPagePage();
-
-        // WHEN: Crear publicar, editar y guardar
-        When.createAndPublishPageEditAndSavePool(3);
-
-        // THEN: Publicar la página
-        Then.validatePageWasEditedAndCreated();
-    });
-
-         it('Escenario 36: Crear una página con HTML que incluya tablas.', () => {
-        // THEN: Visitar pagina de posts
-        Given.givenNavigateToPagePage();
-
-        // WHEN: Crear y publicar post
-        When.createAndPublishPageWithHtmlPool(3);
-
-        // THEN: Verificar post publicado
-        Then. seePagePublishedWithHtmlName("Pagina con tabla HTML")
-    });
-         it('Escenario 37: Crear una página con HTML que incluya una lista.', () => {
-        // THEN: Visitar pagina de posts
-        Given.givenNavigateToPagePage();
-
-        // WHEN: Crear y publicar post
-        When.createAndPublishPageWithHtmlPool(4);
-
-        // THEN: Verificar post publicado
-        Then. seePagePublishedWithHtmlName("Página con lista HTML")
-    });
-
-         it('Escenario 38: Crear una página con HTML con imagen y parrafo.', () => {
-        // THEN: Visitar pagina de posts
-        Given.givenNavigateToPagePage();
-
-        // WHEN: Crear y publicar post
-        When.createAndPublishPageWithHtmlPool(5);
-
-        // THEN: Verificar post publicado
-        Then. seePagePublishedWithHtmlName("The Rise of Sustainable Energy")
-    });
-
-         it('Escenario 39: Crear una página con HTML  con enlace y párrafos.', () => {
-        // THEN: Visitar pagina de posts
-        Given.givenNavigateToPagePage();
-
-        // WHEN: Crear y publicar post
-        When.createAndPublishPageWithHtmlPool(6);
-
-        // THEN: Verificar post publicado
-        Then. seePagePublishedWithHtmlName("Exploring the Depths of the Ocean")
-    });
-         it('Escenario 40: Crear una página con  HTML con video y formulario de contacto.', () => {
-        // THEN: Visitar pagina de posts
-        Given.givenNavigateToPagePage();
-
-        // WHEN: Crear y publicar post
-        When.createAndPublishPageWithHtmlPool(7);
-
-        // THEN: Verificar post publicado
-        Then. seePagePublishedWithHtmlName("How to Start Your Own Business")
-    });
 
 });
